9db1f881dd1ff3efc936fbce6517b788c38e3260
seriktest <96250381+seriktest@users.noreply.github.com>
2023-02-21 09:03:13 +0300

    Initial commit

---
+++ ConsoleUI/GreetingService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+public class GreetingService
+{
+    private readonly ILogger<GreetingService> _log;
+    private readonly IConfiguration _config;
+
+    public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+    {
+        _log = log;
+        _config = config;
+    }
+    
+    public void Run()
+    {
+        for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+        {
+            _log.LogInformation("Run number {runNumber}", i);
+        }
+    }
+}
